eb7a61a8a0289d713efbeb6fbada0138ef34a29a
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2021-05-30 14:13:58 +0200

    End of work in 30.05.2021

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-let nav = document.querySelector("nav");
-let fSection = document.querySelector(".fSection")
-fSection.style.paddingTop = `${nav.clientHeight}px`
+'use strict'
+
+let galleryParagraph = document.querySelector(".galleryParagraph")
+let hiddenGallery = document.querySelector("#hG");
+let hiddenSection  = document.querySelector(".hiddenSection")
+galleryParagraph.addEventListener("click", ()=>{
+    hiddenGallery.classList.toggle("hidden");
+    hiddenSection.style.display = "block"
+})
+
+let video = document.querySelector("video");
+let player = document.querySelector(".player")
+player.addEventListener("click", ()=>{
+    if(player.getAttribute("id") === "pause"){
+        video.play();
+        player.setAttribute("id", "play");
+        player.setAttribute("class", "player fas fa-pause");
+    }else if(player.getAttribute("id") === "play"){
+        video.pause();
+        player.setAttribute("id", "pause");
+        player.setAttribute("class", "player fas fa-play");
+    }
+})
